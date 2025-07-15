85e60886eb83909e652b031418920b8c0bb2f992
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-07-15 16:46:42 +0200

    fix api

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -1,9 +1,9 @@
-import { getArticlesFromNotion } from '@/lib/notion';
+import { getPosts } from '@/lib/notion';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const articles = await getArticlesFromNotion();
+    const articles = await getPosts();
     return NextResponse.json({ articles });
   } catch (error) {
     return NextResponse.json({ error: (error as Error).message });
