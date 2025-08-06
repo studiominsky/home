72e74ee9a2f16d776c96a2cfa6ac10657e90743c
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-08-06 21:03:36 +0200

    add header to slug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 // app/blog/[slug]/page.tsx
+import Header from '@/components/Header';
 import { getPost, getPosts } from '@/lib/notion';
 
 export async function generateStaticParams() {
@@ -16,32 +17,36 @@ export default async function BlogPostPage({
   const { frontMatter, html } = await getPost(slug);
 
   return (
-    <article className="container mx-auto px-4 py-8 prose lg:prose-xl">
-      <header className="mb-8">
-        <h1 className="text-4xl font-bold mb-2">
-          {frontMatter.title}
-        </h1>
-        <p className="text-gray-600">{frontMatter.description}</p>
-        <time className="text-sm text-gray-500">
-          Published on{' '}
-          {new Date(frontMatter.date).toLocaleDateString('en-EN', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          })}
-        </time>
-      </header>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-      {frontMatter.headings.length > 0 && (
-        <nav className="mt-12">
-          <h2 className="font-semibold mb-2">On this page</h2>
-          <ul className="list-disc list-inside">
-            {frontMatter.headings.map((h) => (
-              <li key={h}>{h}</li>
-            ))}
-          </ul>
-        </nav>
-      )}
-    </article>
+    <>
+      <Header />
+
+      <article className="mt-[80px] container mx-auto px-4 py-8 prose lg:prose-xl">
+        <header className="mb-8">
+          <h1 className="text-4xl font-bold mb-2">
+            {frontMatter.title}
+          </h1>
+          <p className="text-gray-600">{frontMatter.description}</p>
+          <time className="text-sm text-gray-500">
+            Published on{' '}
+            {new Date(frontMatter.date).toLocaleDateString('en-EN', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </time>
+        </header>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        {frontMatter.headings.length > 0 && (
+          <nav className="mt-12">
+            <h2 className="font-semibold mb-2">On this page</h2>
+            <ul className="list-disc list-inside">
+              {frontMatter.headings.map((h) => (
+                <li key={h}>{h}</li>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </article>
+    </>
   );
 }
