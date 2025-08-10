5d85d47d0a17fee2ced5a33453a4d386dca1706c
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-08-10 22:20:54 +0200

    add default system theme

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -78,6 +78,7 @@ export function ThemeProvider({
     <NextThemesProvider
       attribute="data-theme"
       storageKey="theme"
+      defaultTheme="system"
       {...props}
     >
       <AppThemeProvider>{children}</AppThemeProvider>
