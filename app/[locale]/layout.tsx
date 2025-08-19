ce5caee9e1bbab607519d78c902674224ae95911
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-08-19 20:55:07 +0200

    add i18n, fix layout promise

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,15 +2,11 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/providers/theme-provider';
-
-import '@/styles/globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 
-const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist',
-});
+import '@/styles/globals.css';
 
+const geist = Geist({ subsets: ['latin'], variable: '--font-geist' });
 const mono = Geist_Mono({
   subsets: ['latin'],
   weight: ['400'],
@@ -35,20 +31,25 @@ export const metadata: Metadata = {
   description: 'Web development studio based in Vienna, Austria.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-  params: { locale },
+  params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: 'en' | 'de' }>;
 }) {
+  const { locale } = await params;
+
+  const messages = (await import(`@/messages/${locale}.json`))
+    .default;
+
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
         className={`${geist.variable} ${mono.variable} ${geometric.variable} antialiased bg-[var(--background)] text-[var(--foreground)]`}
       >
-        <NextIntlClientProvider>
-          <ThemeProvider enableSystem={true} defaultTheme="system">
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ThemeProvider enableSystem defaultTheme="system">
             {children}
           </ThemeProvider>
         </NextIntlClientProvider>
