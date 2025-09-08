45e9d110964441b159c98dda40dc67ee74acf326
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-09-08 13:48:23 +0200

    fix heading

--- components/AnimatedText.tsx
+++ components/AnimatedText.tsx
@@ -73,7 +73,7 @@ export default function ContentBlock({
       <Container>
         <h4
           ref={textRef}
-          className="text-black text-center font-geometric text-4xl sm:text-5xl md:text-6xl xl:text-7xl mb-8 leading-tight md:leading-[23px]"
+          className="text-black text-center font-geometric text-4xl sm:text-5xl md:text-6xl lg:text-7xl  mb-8 leading-tight md:leading-23"
         >
           {children}
         </h4>
